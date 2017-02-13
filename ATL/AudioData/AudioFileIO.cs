a2ba393d16e7d214aae3175c6f596cf7fd842ad0
Rob_w <zeugma440@hotmail.com>
2017-02-13 23:54:13 +0100

    Remove gitignore on AudioData folder

---
+++ ATL/AudioData/AudioFileIO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATL.AudioData
+{
+    class AudioFileIO
+    {
+        public String Title;
+        public String Artist;
+        public String Composer;
+        public String Comment;
+        public String Genre;
+        public int IntYear;
+        public String Album;
+        public int Track;
+        public int Disc;
+        public int IntBitRate;
+        public int IntDuration;
+        public int CodecFamily;
+        public int Rating;
+        public bool IsVBR;
+
+        public List<MetaDataIOFactory.PIC_CODE> Pictures;
+
+        public AudioFileIO(String path, MetaDataIOFactory.PictureStreamHandlerDelegate pictureStreamHandler)
+        {
+            // TODO
+        }
+    }
+}
---
+++ ATL/AudioData/IOBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace ATL.AudioData
+{
+    public class IOBase
+    {
+        // Optimal settings
+        public static int bufferSize = 2048;
+        public static FileOptions fileOptions = FileOptions.RandomAccess;
+
+        public static void ChangeFileOptions(FileOptions options)
+        {
+            fileOptions = options;
+        }
+
+        public static void ChangeBufferSize(int bufSize)
+        {
+            bufferSize = bufSize;
+        }
+
+    }
+}
