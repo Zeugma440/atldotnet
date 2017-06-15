7b979a44543dfe3fe8d54c9695adecc77267e061
Z440 <zeugma440@hotmail.com>
2017-06-15 22:29:43 +0200

    Dynamic path generation for playlists that require absolute paths

--- ATL.test/PlaylistReaders/PlaylistTest.cs
+++ ATL.test/PlaylistReaders/PlaylistTest.cs
@@ -1,11 +1,28 @@
 ﻿using ATL.PlaylistReaders;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 
 namespace ATL.test
 {
     [TestClass]
     public class PlaylistTest
     {
+        private string copyFileAndReplace(string location, string placeholder, string replacement)
+        {
+            string testFileLocation = location.Substring(0, location.LastIndexOf('.')) + "_test" + location.Substring(location.LastIndexOf('.'), location.Length- location.LastIndexOf('.'));
+
+            using (StreamWriter s = File.CreateText(testFileLocation))
+            {
+                foreach (string line in File.ReadLines(location))
+                {
+                    s.WriteLine(line.Replace(placeholder, replacement));
+                }
+            }
+
+            return testFileLocation;
+        }
+
+
         [TestMethod]
         public void TestM3U()
         {
@@ -23,14 +40,23 @@ namespace ATL.test
         [TestMethod]
         public void TestXSPF()
         {
-            IPlaylistReader theReader = PlaylistReaders.PlaylistReaderFactory.GetInstance().GetPlaylistReader("../../Resources/playlist.xspf");
+            string testFileLocation = copyFileAndReplace("../../Resources/playlist.xspf", "$PATH", Path.GetFullPath("../../Resources"));
 
-            Assert.IsNotInstanceOfType(theReader, typeof(PlaylistReaders.BinaryLogic.DummyReader));
-            Assert.AreEqual(5, theReader.GetFiles().Count);
-            foreach (string s in theReader.GetFiles())
+            try
             {
-                System.Console.WriteLine(s);
-                Assert.IsTrue(System.IO.File.Exists(s));
+                IPlaylistReader theReader = PlaylistReaders.PlaylistReaderFactory.GetInstance().GetPlaylistReader(testFileLocation);
+
+                Assert.IsNotInstanceOfType(theReader, typeof(PlaylistReaders.BinaryLogic.DummyReader));
+                Assert.AreEqual(5, theReader.GetFiles().Count);
+                foreach (string s in theReader.GetFiles())
+                {
+                    System.Console.WriteLine(s);
+                    Assert.IsTrue(System.IO.File.Exists(s));
+                }
+            }
+            finally
+            {
+                File.Delete(testFileLocation);
             }
         }
 
@@ -51,14 +77,24 @@ namespace ATL.test
         [TestMethod]
         public void TestASX()
         {
-            IPlaylistReader theReader = PlaylistReaders.PlaylistReaderFactory.GetInstance().GetPlaylistReader("../../Resources/playlist.asx");
+            string testFileLocation = copyFileAndReplace("../../Resources/playlist.asx", "$PATH", Path.GetFullPath("../../Resources").Replace("\\","/"));
 
-            Assert.IsNotInstanceOfType(theReader, typeof(PlaylistReaders.BinaryLogic.DummyReader));
-            Assert.AreEqual(4, theReader.GetFiles().Count);
-            foreach (string s in theReader.GetFiles())
+            try
             {
-                System.Console.WriteLine(s);
-                Assert.IsTrue(System.IO.File.Exists(s));
+
+                IPlaylistReader theReader = PlaylistReaders.PlaylistReaderFactory.GetInstance().GetPlaylistReader(testFileLocation);
+
+                Assert.IsNotInstanceOfType(theReader, typeof(PlaylistReaders.BinaryLogic.DummyReader));
+                Assert.AreEqual(4, theReader.GetFiles().Count);
+                foreach (string s in theReader.GetFiles())
+                {
+                    System.Console.WriteLine(s);
+                    Assert.IsTrue(System.IO.File.Exists(s));
+                }
+            }
+            finally
+            {
+                File.Delete(testFileLocation);
             }
         }
 
