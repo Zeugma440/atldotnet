7bfda5eb0b4c2372dbd3fc32fca055f06c5ddb22
Zeugma440 <zeugma440@hotmail.com>
2017-08-02 19:15:07 +0200

    Revert to classic notation for members that return NotImplementedException

--- ATL/AudioData/CrossMetadataReader.cs
+++ ATL/AudioData/CrossMetadataReader.cs
@@ -368,12 +368,24 @@ namespace ATL.AudioData
             }
         }
 
-        public long Offset => throw new NotImplementedException();
+        public long Offset
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
 
-        public byte[] CoreSignature => throw new NotImplementedException();
+        public byte[] CoreSignature
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
 
-        public bool Read(BinaryReader source, MetaDataIO.ReadTagParams readTagParams) => throw new NotImplementedException();
+        public bool Read(BinaryReader source, MetaDataIO.ReadTagParams readTagParams) { throw new NotImplementedException(); }
 
-        public long Write(BinaryReader r, BinaryWriter w, TagData tag) => throw new NotImplementedException();
+        public long Write(BinaryReader r, BinaryWriter w, TagData tag) { throw new NotImplementedException(); }
     }
 }
