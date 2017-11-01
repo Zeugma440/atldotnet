5d970bd7e023e82dd6e7c8e9c4c5a5f07e323b49
Z440 <zeugma440@hotmail.com>
2017-11-01 08:38:58 +0100

    OFR : Cleanup unused code

--- ATL/AudioData/IO/OptimFROG.cs
+++ ATL/AudioData/IO/OptimFROG.cs
@@ -11,6 +11,7 @@ namespace ATL.AudioData.IO
     /// </summary>
 	class OptimFrog : IAudioDataIO
 	{
+        private const string OFR_SIGNATURE = "OFR ";
 
 		private static readonly string[] OFR_COMPRESSION = new string[10] 
 		{
@@ -53,7 +54,7 @@ namespace ATL.AudioData.IO
 		}
 
     
-		private TOfrHeader FHeader = new TOfrHeader();
+		private TOfrHeader header = new TOfrHeader();
 
         private double bitrate;
         private double duration;
@@ -61,7 +62,7 @@ namespace ATL.AudioData.IO
         private SizeInfo sizeInfo;
         private readonly string filePath;
 
-
+        /* Unused for now
         public TOfrHeader Header // OptimFROG header
 		{
 			get { return this.FHeader; }
@@ -91,6 +92,7 @@ namespace ATL.AudioData.IO
         {
             get { return this.getCompressionRatio(); }
         }
+        */
 
 
         // ---------- INFORMATIVE INTERFACE IMPLEMENTATIONS & MANDATORY OVERRIDES
@@ -140,7 +142,7 @@ namespace ATL.AudioData.IO
             duration = 0;
             bitrate = 0;
 
-            FHeader.Reset();
+            header.Reset();
 		}
 
         public OptimFrog(string filePath)
@@ -152,75 +154,78 @@ namespace ATL.AudioData.IO
 
         // ---------- SUPPORT METHODS
 
-        private bool getValid()
-		{
-			return (
-				StreamUtils.StringEqualsArr("OFR ", FHeader.ID) &&
-				(FHeader.SampleRate > 0) &&
-				((0 <= FHeader.SampleType) && (FHeader.SampleType <= 10)) &&
-				((0 <= FHeader.ChannelMode) &&(FHeader.ChannelMode <= 1)) &&
-				((0 <= FHeader.CompressionID >> 3) && (FHeader.CompressionID >> 3 <= 9)) );
-		}
+        /* Unused for now
+    private bool getValid()
+    {
+        return (
+            StreamUtils.StringEqualsArr("OFR ", FHeader.ID) &&
+            (FHeader.SampleRate > 0) &&
+            ((0 <= FHeader.SampleType) && (FHeader.SampleType <= 10)) &&
+            ((0 <= FHeader.ChannelMode) &&(FHeader.ChannelMode <= 1)) &&
+            ((0 <= FHeader.CompressionID >> 3) && (FHeader.CompressionID >> 3 <= 9)) );
+    }
 
-		private String getVersion()
-		{
-			// Get encoder version
-			return  ( ((FHeader.EncoderID >> 4) + 4500) / 1000 ).ToString().Substring(0,5); // Pas exactement...
-		}
+    private String getVersion()
+    {
+        // Get encoder version
+        return  ( ((FHeader.EncoderID >> 4) + 4500) / 1000 ).ToString().Substring(0,5); // Pas exactement...
+    }
 
-		private String getCompression()
-		{
-			// Get compression level
-			return OFR_COMPRESSION[FHeader.CompressionID >> 3];
-		}
+    private String getCompression()
+    {
+        // Get compression level
+        return OFR_COMPRESSION[FHeader.CompressionID >> 3];
+    }
 
-		private sbyte getBits()
-		{
-			// Get number of bits per sample
-			return OFR_BITS[FHeader.SampleType];
-		}
+    private sbyte getBits()
+    {
+        // Get number of bits per sample
+        return OFR_BITS[FHeader.SampleType];
+    }
 
-		private String getChannelMode()
-		{
-			// Get channel mode
-			return OFR_CHANNELMODE[FHeader.ChannelMode];
-		}
+    private String getChannelMode()
+    {
+        // Get channel mode
+        return OFR_CHANNELMODE[FHeader.ChannelMode];
+    }
 
-		private long getSamples()
+    private double getCompressionRatio()
+    {
+        // Get compression ratio
+        if (getValid())
+            return sizeInfo.FileSize /
+                (getSamples() * (header.ChannelMode+1) * Math.Abs(getBits()) / 8 + 44) * 100;
+        else
+            return 0;
+    }
+
+    */
+
+        private long getSamples()
 		{
 			// Get number of samples
-			return ( ((Header.Length >> Header.ChannelMode) * 0x00000001) +
-				((Header.HiLength >> Header.ChannelMode) * 0x00010000) );
+			return ( ((header.Length >> header.ChannelMode) * 0x00000001) +
+				((header.HiLength >> header.ChannelMode) * 0x00010000) );
 		}
 
 		private double getDuration()
 		{
 			double nbSamples = (double)getSamples();
 			// Get song duration
-			if (FHeader.SampleRate > 0)
-				return nbSamples / FHeader.SampleRate;
+			if (header.SampleRate > 0)
+				return nbSamples / header.SampleRate;
 			else
 				return 0;
 		}
 
 		private int getSampleRate()
 		{
-			return Header.SampleRate;
-		}
-
-		private double getCompressionRatio()
-		{
-			// Get compression ratio
-			if (getValid())
-				return sizeInfo.FileSize /
-					(getSamples() * (FHeader.ChannelMode+1) * Math.Abs(getBits()) / 8 + 44) * 100;
-			else
-				return 0;
+			return header.SampleRate;
 		}
 
         private double getBitrate()
         {
-            return ((sizeInfo.FileSize - FHeader.Size - sizeInfo.TotalTagSize) * 8 / (Duration));
+            return ((sizeInfo.FileSize - header.Size - sizeInfo.TotalTagSize) * 8 / (Duration));
         }
 
         public bool Read(BinaryReader source, SizeInfo sizeInfo, MetaDataIO.ReadTagParams readTagParams)
@@ -232,17 +237,17 @@ namespace ATL.AudioData.IO
 			// Read header data
 			source.BaseStream.Seek(sizeInfo.ID3v2Size, SeekOrigin.Begin);
 
-			FHeader.ID = source.ReadChars(4);
-			FHeader.Size = source.ReadUInt32();
-			FHeader.Length = source.ReadUInt32();
-			FHeader.HiLength = source.ReadUInt16();
-			FHeader.SampleType = source.ReadByte();
-			FHeader.ChannelMode = source.ReadByte();
-			FHeader.SampleRate = source.ReadInt32();
-			FHeader.EncoderID = source.ReadUInt16();
-			FHeader.CompressionID = source.ReadByte();
+			header.ID = source.ReadChars(4);
+			header.Size = source.ReadUInt32();
+			header.Length = source.ReadUInt32();
+			header.HiLength = source.ReadUInt16();
+			header.SampleType = source.ReadByte();
+			header.ChannelMode = source.ReadByte();
+			header.SampleRate = source.ReadInt32();
+			header.EncoderID = source.ReadUInt16();
+			header.CompressionID = source.ReadByte();
 
-            if (StreamUtils.StringEqualsArr("OFR ", FHeader.ID))
+            if (StreamUtils.StringEqualsArr(OFR_SIGNATURE, header.ID))
             {
                 result = true;
                 duration = getDuration();
