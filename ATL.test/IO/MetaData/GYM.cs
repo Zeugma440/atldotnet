35289c4ed29bdaebd616fc2d0ac5979108ccad22
Z440 <zeugma440@hotmail.com>
2017-12-03 21:04:13 +0100

    Use generic unit test code for ID3v1

--- ATL.test/IO/MetaData/GYM.cs
+++ ATL.test/IO/MetaData/GYM.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ATL.AudioData;
-using System.IO;
 using System.Collections.Generic;
 
 namespace ATL.test.IO.MetaData
@@ -44,138 +43,12 @@ namespace ATL.test.IO.MetaData
         public void TagIO_RW_GYM_Empty()
         {
             test_RW_Empty(emptyFile, true, true, true);
-            /*
-            ConsoleLogger log = new ConsoleLogger();
-
-            // Source : totally metadata-free SPC
-            string location = TestUtils.GetResourceLocationRoot() + emptyFile;
-            string testFileLocation = TestUtils.GetTempTestFile(emptyFile);
-            AudioDataManager theFile = new AudioDataManager(AudioData.AudioDataIOFactory.GetInstance().GetDataReader(testFileLocation));
-
-
-            // Check that it is indeed metadata-free
-            Assert.IsTrue(theFile.ReadFromFile());
-
-            Assert.IsNotNull(theFile.NativeTag);
-            // Assert.IsFalse(theFile.NativeTag.Exists); Tag data is embedded in GYM header => can never be nonexistent
-
-            // Construct a new tag
-            TagData theTag = new TagData();
-            theTag.Title = "Test !!";
-            theTag.Album = "Album";
-            theTag.Copyright = "Artist";
-            theTag.Comment = "This is a test";
-
-            // Add the new tag and check that it has been indeed added with all the correct information
-            Assert.IsTrue(theFile.UpdateTagInFile(theTag, MetaDataIOFactory.TAG_NATIVE));
-
-            Assert.IsTrue(theFile.ReadFromFile());
-
-            Assert.IsNotNull(theFile.NativeTag);
-            Assert.IsTrue(theFile.NativeTag.Exists);
-
-            Assert.AreEqual("Test !!", theFile.NativeTag.Title);
-            Assert.AreEqual("Album", theFile.NativeTag.Album);
-            Assert.AreEqual("Artist", theFile.NativeTag.Copyright);
-            Assert.AreEqual("This is a test", theFile.NativeTag.Comment);
-
-            // Remove the tag and check that it has been indeed removed
-            Assert.IsTrue(theFile.RemoveTagFromFile(MetaDataIOFactory.TAG_NATIVE));
-
-            Assert.IsTrue(theFile.ReadFromFile());
-
-            Assert.IsNotNull(theFile.NativeTag);
-            // Assert.IsFalse(theFile.NativeTag.Exists); Tag data is embedded in GYM header => can never be nonexistent
-
-
-            // Check that the resulting file (working copy that has been tagged, then untagged) remains identical to the original file (i.e. no byte lost nor added)
-            FileInfo originalFileInfo = new FileInfo(location);
-            FileInfo testFileInfo = new FileInfo(testFileLocation);
-
-            Assert.AreEqual(originalFileInfo.Length, testFileInfo.Length);
-
-            string originalMD5 = TestUtils.GetFileMD5Hash(location);
-            string testMD5 = TestUtils.GetFileMD5Hash(testFileLocation);
-
-            Assert.IsTrue(originalMD5.Equals(testMD5));
-
-            // Get rid of the working copy
-            File.Delete(testFileLocation);
-            */
         }
 
         [TestMethod]
         public void tagIO_RW_GYM_Existing()
         {
             test_RW_Existing(notEmptyFile, 0, true, true);
-            /*
-            ConsoleLogger log = new ConsoleLogger();
-
-            // Source : file with existing tag incl. unsupported field (dumper)
-            string location = TestUtils.GetResourceLocationRoot() + notEmptyFile;
-            string testFileLocation = TestUtils.GetTempTestFile(notEmptyFile);
-            AudioDataManager theFile = new AudioDataManager(AudioData.AudioDataIOFactory.GetInstance().GetDataReader(testFileLocation));
-
-            // Add a new supported field and a new supported picture
-            Assert.IsTrue(theFile.ReadFromFile());
-
-            TagData theTag = new TagData();
-            theTag.Copyright = "1999";
-
-
-            // Add the new tag and check that it has been indeed added with all the correct information
-            Assert.IsTrue(theFile.UpdateTagInFile(theTag, MetaDataIOFactory.TAG_NATIVE));
-
-            readExistingTagsOnFile(theFile, "1999");
-
-
-            // Remove the additional supported field
-            theTag = new TagData();
-            theTag.Copyright = "1990 Sega";
-
-            // Add the new tag and check that it has been indeed added with all the correct information
-            Assert.IsTrue(theFile.UpdateTagInFile(theTag, MetaDataIOFactory.TAG_NATIVE));
-
-            readExistingTagsOnFile(theFile);
-
-
-            // Check that the resulting file (working copy that has been tagged, then untagged) remains identical to the original file (i.e. no byte lost nor added)
-            FileInfo originalFileInfo = new FileInfo(location);
-            FileInfo testFileInfo = new FileInfo(testFileLocation);
-
-            Assert.AreEqual(originalFileInfo.Length, testFileInfo.Length);
-
-            string originalMD5 = TestUtils.GetFileMD5Hash(location);
-            string testMD5 = TestUtils.GetFileMD5Hash(testFileLocation);
-
-            Assert.IsTrue(originalMD5.Equals(testMD5));
-
-            // Get rid of the working copy
-            File.Delete(testFileLocation);
-            */
         }
-        /*
-        private void readExistingTagsOnFile(AudioDataManager theFile, string copyStr = "1990 Sega")
-        {
-            pictures.Clear();
-            Assert.IsTrue(theFile.ReadFromFile(new TagData.PictureStreamHandlerDelegate(this.readPictureData), true));
-
-            Assert.IsNotNull(theFile.NativeTag);
-            Assert.IsTrue(theFile.NativeTag.Exists);
-
-            // Supported fields
-            Assert.AreEqual("The Source of Evilness", theFile.NativeTag.Title);
-            Assert.AreEqual("Arrow Flash", theFile.NativeTag.Album);
-            Assert.AreEqual("Last Stage Music", theFile.NativeTag.Comment);
-            Assert.AreEqual(copyStr, theFile.NativeTag.Copyright);
-
-            // Unsupported fields
-            Assert.IsTrue(theFile.NativeTag.AdditionalFields.Keys.Contains("EMULATOR"));
-            Assert.AreEqual("Magasis", theFile.NativeTag.AdditionalFields["EMULATOR"]); // Is there a mapping between that and actual system names ?
-
-            Assert.IsTrue(theFile.NativeTag.AdditionalFields.Keys.Contains("DUMPER"));
-            Assert.AreEqual("Guy", theFile.NativeTag.AdditionalFields["DUMPER"]);
-        }
-        */
     }
 }
--- ATL.test/IO/MetaData/ID3v1.cs
+++ ATL.test/IO/MetaData/ID3v1.cs
@@ -6,131 +6,48 @@ using System.IO;
 namespace ATL.test.IO.MetaData
 {
     [TestClass]
-    public class ID3v1
+    public class ID3v1 : MetaIOTest
     {
-        [TestMethod]
-        public void TagIO_RW_ID3v1_Empty()
+        public ID3v1()
         {
-            ConsoleLogger log = new ConsoleLogger();
+            emptyFile = "MP3/empty.mp3";
+            notEmptyFile = "MP3/id3v1.mp3";
+            tagType = MetaDataIOFactory.TAG_ID3V1;
 
-            // Source : tag-free MP3
-            string resourceName = "MP3/empty.mp3";
-            string location = TestUtils.GetResourceLocationRoot() + resourceName;
-            string testFileLocation = TestUtils.GetTempTestFile(resourceName);
-            AudioDataManager theFile = new AudioDataManager( AudioData.AudioDataIOFactory.GetInstance().GetDataReader(testFileLocation) );
+            // Initialize specific test data
+            testData = new TagData();
 
-
-            // Check that it is indeed tag-free
-            Assert.IsTrue(theFile.ReadFromFile());
-            
-            Assert.IsNotNull(theFile.ID3v1);
-            Assert.IsFalse(theFile.ID3v1.Exists);
-
-
-            // Construct a new tag
-            TagData theTag = new TagData();
-            theTag.Title = "Test !!";
-            theTag.Album = "Album";
-            theTag.Artist = "Artist";
-            theTag.Comment = "This is a test";
-            theTag.RecordingYear = "2008/01/01";
-            theTag.Genre = "Merengue";
-            theTag.TrackNumber = "01/01";
-
-
-            // Add the new tag and check that it has been indeed added with all the correct information
-            Assert.IsTrue(theFile.UpdateTagInFile(theTag, MetaDataIOFactory.TAG_ID3V1));
-            Assert.IsTrue(theFile.ReadFromFile());
-
-            Assert.IsNotNull(theFile.ID3v1);
-            Assert.IsTrue(theFile.ID3v1.Exists);
-
-            Assert.AreEqual("Test !!", theFile.ID3v1.Title);
-            Assert.AreEqual("Album", theFile.ID3v1.Album);
-            Assert.AreEqual("Artist", theFile.ID3v1.Artist);
-            Assert.AreEqual("This is a test", theFile.ID3v1.Comment);
-            Assert.AreEqual("2008", theFile.ID3v1.Year);
-            Assert.AreEqual("Merengue", theFile.ID3v1.Genre);
-            Assert.AreEqual(1, theFile.ID3v1.Track);
-
-
-            // Remove the tag and check that it has been indeed removed
-            Assert.IsTrue(theFile.RemoveTagFromFile(MetaDataIOFactory.TAG_ID3V1));
-
-            Assert.IsTrue(theFile.ReadFromFile());
-                
-            Assert.IsNotNull(theFile.ID3v1);
-            Assert.IsFalse(theFile.ID3v1.Exists);
-
-
-            // Check that the resulting file (working copy that has been tagged, then untagged) remains identical to the original file (i.e. no byte lost nor added)
-            FileInfo originalFileInfo = new FileInfo(location);
-            FileInfo testFileInfo = new FileInfo(testFileLocation);
-
-            Assert.AreEqual(originalFileInfo.Length, testFileInfo.Length);
-
-            string originalMD5 = TestUtils.GetFileMD5Hash(location);
-            string testMD5 = TestUtils.GetFileMD5Hash(testFileLocation);
-
-            Assert.IsTrue(originalMD5.Equals(testMD5));
-
-            // Get rid of the working copy
-            File.Delete(testFileLocation);
-        }
-
-        [TestMethod]
-        public void TagIO_RW_ID3v1_Existing()
-        {
-            ConsoleLogger log = new ConsoleLogger();
-
-            // Source : MP3 with existing tag
-            String testFileLocation = TestUtils.GetTempTestFile("MP3/id3v1.mp3");
-            AudioDataManager theFile = new AudioDataManager( AudioData.AudioDataIOFactory.GetInstance().GetDataReader(testFileLocation) );
-
-            // Construct a new tag; only rewrite Genre and track number
-            TagData theTag = new TagData();
-            theTag.Genre = "Merengue";
-            theTag.TrackNumber = "002/04";
-
-            // Add the new tag and check that it has been indeed added with all the correct information
-            Assert.IsTrue(theFile.UpdateTagInFile(theTag, MetaDataIOFactory.TAG_ID3V1));
-
-            Assert.IsTrue(theFile.ReadFromFile());
-
-            Assert.IsNotNull(theFile.ID3v1);
-            Assert.IsTrue(theFile.ID3v1.Exists);
-            Assert.AreEqual("Title", theFile.ID3v1.Title);
-            Assert.AreEqual("?", theFile.ID3v1.Album);
-            Assert.AreEqual("Artist", theFile.ID3v1.Artist);
-            Assert.AreEqual("Test!", theFile.ID3v1.Comment);
-            Assert.AreEqual("2017", theFile.ID3v1.Year);
-            Assert.AreEqual("Merengue", theFile.ID3v1.Genre);
-            Assert.AreEqual(2, theFile.ID3v1.Track);
-
-            // Get rid of the working copy
-            File.Delete(testFileLocation);
+            testData.Title = "Title";
+            testData.Album = "?";
+            testData.Artist = "Artist";
+            testData.Comment = "Test!";
+            testData.RecordingYear = "2017";
+            testData.Genre = "Bluegrass";
+            testData.TrackNumber = "22";
         }
 
         [TestMethod]
         public void TagIO_R_ID3v1()
         {
-            // Source : MP3 with existing tag
-            String location = TestUtils.GetResourceLocationRoot()+ "MP3/id3v1.mp3";
-            AudioDataManager theFile = new AudioDataManager( AudioData.AudioDataIOFactory.GetInstance().GetDataReader(location) );
+            ConsoleLogger log = new ConsoleLogger();
 
-            Assert.IsTrue(theFile.ReadFromFile());
+            string location = TestUtils.GetResourceLocationRoot() + notEmptyFile;
+            AudioDataManager theFile = new AudioDataManager(AudioData.AudioDataIOFactory.GetInstance().GetDataReader(location));
 
-            Assert.IsNotNull(theFile.ID3v1);
-            Assert.IsTrue(theFile.ID3v1.Exists);
-
-            // Supported fields
-            Assert.AreEqual("Title", theFile.ID3v1.Title);
-            Assert.AreEqual("?", theFile.ID3v1.Album);
-            Assert.AreEqual("Artist", theFile.ID3v1.Artist);
-            Assert.AreEqual("Test!", theFile.ID3v1.Comment);
-            Assert.AreEqual("2017", theFile.ID3v1.Year);
-            Assert.AreEqual("Bluegrass", theFile.ID3v1.Genre);
-            Assert.AreEqual(22, theFile.ID3v1.Track);
+            readExistingTagsOnFile(theFile);
         }
+
+        [TestMethod]
+        public void TagIO_RW_ID3v1_Empty()
+        {
+            test_RW_Empty(emptyFile, true, true, true);
+        }
+
+        [TestMethod]
+        public void TagIO_RW_ID3v1_Existing()
+        {
+            test_RW_Existing(notEmptyFile, 0, true, true);
+        }
+
     }
 }
