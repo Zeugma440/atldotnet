0bebcdcd9f34b16c3554ec4a4c6eaaebc391c551
Zeugma440 <Zeugma440@users.noreply.github.com>
2019-03-09 14:46:46 +0100

    ChannelsArrangement baseline

---
+++ ATL/Entities/ChannelsArrangement.cs
@@ -0,0 +1,22 @@
+﻿namespace ATL
+{
+    public class ChannelsArrangements
+    {
+        public static ChannelsArrangement Mono = new ChannelsArrangement(1, "Mono (1/0)");
+        public static ChannelsArrangement Stereo = new ChannelsArrangement(2, "Stereo (2/0)");
+
+        public class ChannelsArrangement {
+            private int nbChannels;
+            private string description;
+
+            public ChannelsArrangement(int nbChannels, string description)
+            {
+                this.nbChannels = nbChannels;
+                this.description = description;
+            }
+
+            public string Description { get => description; set => description = value; }
+            public int NbChannels { get => nbChannels; set => nbChannels = value; }
+        }
+    }
+}
