2edf43c38faff5caeda4e27d7bf7d8baa0c6f58b
Zeugma440 <Zeugma440@users.noreply.github.com>
2019-03-10 09:24:27 +0100

    Housekeeping

--- ATL/Entities/Format.cs
+++ ATL/Entities/Format.cs
@@ -1,68 +1,57 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace ATL
 {
-	/// <summary>
-	/// Describes a file format
-	/// </summary>
-	public class Format : IEnumerable
-	{
-		// Name of the format
-		protected string fName;
-		// ID of the format
-		protected int fID;
+    /// <summary>
+    /// Describes a file format
+    /// </summary>
+    public class Format : IEnumerable
+    {
+        // Name of the format
+        protected string fName;
+        // ID of the format
+        protected int fID;
         // MIME type of the format
         protected IDictionary<string, int> mimeList;
         // List of file extensions proper to this format
-        protected IDictionary<string,int> extList;
-		// true if the format is readable by ATL
-		protected bool fReadable;
+        protected IDictionary<string, int> extList;
+        // true if the format is readable by ATL
+        protected bool fReadable;
 
-        public Format() { }
 
-		public Format(string iName)
-		{
+        public Format(string iName)
+        {
             init(iName);
-		}
+        }
 
         protected void init(string iName)
         {
             fName = iName;
-			fReadable = true;
-			extList = new Dictionary<string,int>();
+            fReadable = true;
+            extList = new Dictionary<string, int>();
             mimeList = new Dictionary<string, int>();
         }
 
-        protected void copyFrom(Format iFormat)
+        public string Name
         {
-            this.fName = iFormat.fName;
-            this.fID = iFormat.fID;
-            this.fReadable = iFormat.fReadable;
-            this.extList = new Dictionary<string,int>(iFormat.extList);
-            this.mimeList = new Dictionary<string, int>(iFormat.mimeList);
+            get { return fName; }
+            set { fName = value; }
         }
 
-		public String Name
-		{
-			get { return fName; }
-			set { fName = value; }
-		}
+        public int ID
+        {
+            get { return fID; }
+            set { fID = value; }
+        }
 
-		public int ID
-		{
-			get { return fID; }
-			set { fID = value; }
-		}
+        public bool Readable
+        {
+            get { return fReadable; }
+            set { fReadable = value; }
+        }
 
-		public bool Readable
-		{
-			get { return fReadable; }
-			set { fReadable = value; }
-		}
-
-        public ICollection<String> MimeList
+        public ICollection<string> MimeList
         {
             get { return mimeList.Keys; }
         }
@@ -71,10 +60,10 @@ namespace ATL
 
         // NB : Same principle as in Collection		
 
-        public IEnumerator GetEnumerator() 
-		{
-			return extList.Keys.GetEnumerator();
-		}
+        public IEnumerator GetEnumerator()
+        {
+            return extList.Keys.GetEnumerator();
+        }
 
         #endregion
 
@@ -93,15 +82,15 @@ namespace ATL
 
         // Adds the extension ext to the extensions list of this Format
         public void AddExtension(string ext)
-		{
-			if ( !extList.ContainsKey(ext.ToLower()) )
-				extList.Add(ext.ToLower(),0);
-		}
+        {
+            if (!extList.ContainsKey(ext.ToLower()))
+                extList.Add(ext.ToLower(), 0);
+        }
 
-		// Tests if the extension ext is a valid extension of the current Format
-		public bool IsValidExtension(string ext)
-		{
-			return extList.ContainsKey(ext.ToLower());
-		}
-	}
+        // Tests if the extension ext is a valid extension of the current Format
+        public bool IsValidExtension(string ext)
+        {
+            return extList.ContainsKey(ext.ToLower());
+        }
+    }
 }
