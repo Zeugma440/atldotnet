d4e8f0873ad201307df7bff1ec18d305df95b16c
Zeugma440 <Zeugma440@users.noreply.github.com>
2020-01-24 21:08:53 +0100

    Test for more MP4/M4A error messages

--- ATL.test/IO/AudioData/AudioDataExceptions.cs
+++ ATL.test/IO/AudioData/AudioDataExceptions.cs
@@ -10,12 +10,12 @@ namespace ATL.test.IO
     [TestClass]
     public class AudioDataExceptions
     {
-        private void audio_X_AAC_MP4_Atom(string atom, string atomCaption = null)
+        private void audio_X_AAC_MP4_Atom(string resourceName, string atom, int logLevel = Log.LV_ERROR, string atomCaption = null)
         {
             if (null == atomCaption) atomCaption = atom;
 
             ArrayLogger log = new ArrayLogger();
-            string resource = "AAC/mp4.m4a";
+            string resource = "AAC/" + resourceName;
             string testFileLocation = TestUtils.CopyAsTempTestFile(resource);
 
             using (FileStream fs = new FileStream(testFileLocation, FileMode.Open, FileAccess.ReadWrite, FileShare.Read))
@@ -23,11 +23,16 @@ namespace ATL.test.IO
                 StreamUtils.FindSequence(fs, Utils.Latin1Encoding.GetBytes(atom));
                 fs.Seek(-1, SeekOrigin.Current);
                 fs.WriteByte(0);
+                if (StreamUtils.FindSequence(fs, Utils.Latin1Encoding.GetBytes(atom)))
+                {
+                    fs.Seek(-1, SeekOrigin.Current);
+                    fs.WriteByte(0);
+                }
 
                 new Track(fs, ".mp4");
-                IList<LogItem> logItems = log.GetAllItems(Log.LV_ERROR);
+                IList<LogItem> logItems = log.GetAllItems(logLevel);
                 Assert.AreEqual(1, logItems.Count);
-                Assert.AreEqual(atomCaption + " atom could not be found; aborting read", logItems[0].Message);
+                Assert.IsTrue(logItems[0].Message.Contains(atomCaption + " atom could not be found"));
             }
 
             // Get rid of the working copy
@@ -37,21 +42,22 @@ namespace ATL.test.IO
         [TestMethod]
         public void Audio_X_AAC_MP4()
         {
-            audio_X_AAC_MP4_Atom("moov");
-            audio_X_AAC_MP4_Atom("mvhd");
-            audio_X_AAC_MP4_Atom("trak");
-//            audio_X_AAC_MP4_Atom("mdia"); TODO adjust error message - track is now skipped
-            //            audio_X_AAC_MP4_Atom("mdhd", "mdia.mdhd");
-            audio_X_AAC_MP4_Atom("hdlr", "mdia.hdlr");
-            audio_X_AAC_MP4_Atom("minf", "mdia.minf");
-            audio_X_AAC_MP4_Atom("stbl", "mdia.minf.stbl");
-            audio_X_AAC_MP4_Atom("stsd");
-            //            audio_X_AAC_MP4_Atom("stts");
-            //            audio_X_AAC_MP4_Atom("stsc");
-            audio_X_AAC_MP4_Atom("stsz");
-//            audio_X_AAC_MP4_Atom("udta"); optional atim
-//            audio_X_AAC_MP4_Atom("ilst"); optional atom
-            audio_X_AAC_MP4_Atom("mdat");
+            audio_X_AAC_MP4_Atom("mp4.m4a","moov");
+            audio_X_AAC_MP4_Atom("mp4.m4a", "mvhd");
+            audio_X_AAC_MP4_Atom("mp4.m4a", "trak");
+            audio_X_AAC_MP4_Atom("mp4.m4a", "mdia", Log.LV_WARNING);
+            audio_X_AAC_MP4_Atom("mp4.m4a", "hdlr", Log.LV_ERROR, "mdia.hdlr");
+            audio_X_AAC_MP4_Atom("mp4.m4a", "minf", Log.LV_ERROR, "mdia.minf");
+            audio_X_AAC_MP4_Atom("mp4.m4a", "stbl", Log.LV_ERROR, "mdia.minf.stbl");
+            audio_X_AAC_MP4_Atom("mp4.m4a", "stsd");
+            audio_X_AAC_MP4_Atom("mp4.m4a", "stsz");
+            audio_X_AAC_MP4_Atom("mp4.m4a", "udta", Log.LV_WARNING);
+            audio_X_AAC_MP4_Atom("mp4.m4a", "ilst", Log.LV_WARNING);
+            audio_X_AAC_MP4_Atom("mp4.m4a", "mdat");
+
+            audio_X_AAC_MP4_Atom("chapters_QT.m4v", "mdhd");
+            audio_X_AAC_MP4_Atom("chapters_QT.m4v", "stts");
+            audio_X_AAC_MP4_Atom("chapters_QT.m4v", "stsc");
         }
 
         [TestMethod]
