0acc212e0ca8f601db64ce1aa1b7ad1117b157d0
Zeugma440 <Zeugma440@users.noreply.github.com>
2020-02-16 09:24:05 +0100

    Remove obsolete playlist classes

--- ATL.test/Playlist/PlaylistIOTest.cs
+++ ATL.test/Playlist/PlaylistIOTest.cs
@@ -2,6 +2,7 @@
 using ATL.Playlist;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.IO;
 using static ATL.Logging.Log;
 
 namespace ATL.test.IO.Playlist
@@ -50,6 +51,171 @@ namespace ATL.test.IO.Playlist
                 IList<LogItem> logItems = log.GetAllItems(Log.LV_ERROR);
                 Assert.AreEqual(1, logItems.Count);
                 Assert.IsTrue(logItems[0].Message.Contains("efiufhziuefizeub.m3u")); // Can't do much more than than because the exception message is localized
+            }
+        }
+
+        [TestMethod]
+        public void PL_Common()
+        {
+            IPlaylistIO theReader = PlaylistIOFactory.GetInstance().GetPlaylistIO(TestUtils.GetResourceLocationRoot() + "_Playlists/playlist_simple.m3u");
+
+            Assert.AreEqual(TestUtils.GetResourceLocationRoot() + "_Playlists/playlist_simple.m3u", theReader.Path);
+
+            ArrayLogger log = new ArrayLogger();
+            try
+            {
+                theReader = PlaylistIOFactory.GetInstance().GetPlaylistIO(TestUtils.GetResourceLocationRoot() + "_Playlists/efiufhziuefizeub.m3u");
+                IList<string> files = theReader.FilePaths;
+                Assert.Fail();
+            }
+            catch
+            {
+                IList<LogItem> logItems = log.GetAllItems(Log.LV_ERROR);
+                Assert.AreEqual(1, logItems.Count);
+                Assert.IsTrue(logItems[0].Message.Contains("efiufhziuefizeub.m3u")); // Can't do much more than than because the exception message is localized
+            }
+        }
+
+        [TestMethod]
+        public void PL_M3U()
+        {
+            IPlaylistIO theReader = PlaylistIOFactory.GetInstance().GetPlaylistIO(TestUtils.GetResourceLocationRoot() + "_Playlists/playlist_simple.m3u");
+
+            Assert.AreEqual(1, theReader.FilePaths.Count);
+            foreach (string s in theReader.FilePaths)
+            {
+                Assert.IsTrue(System.IO.File.Exists(s));
+            }
+
+            IList<KeyValuePair<string, string>> replacements = new List<KeyValuePair<string, string>>();
+            string resourceRoot = TestUtils.GetResourceLocationRoot(false);
+            replacements.Add(new KeyValuePair<string, string>("$PATH", resourceRoot));
+            replacements.Add(new KeyValuePair<string, string>("$NODISK_PATH", resourceRoot.Substring(2, resourceRoot.Length - 2)));
+
+            string testFileLocation = TestUtils.CopyFileAndReplace(TestUtils.GetResourceLocationRoot() + "_Playlists/playlist_fullPath.m3u", replacements);
+            try
+            {
+                theReader = PlaylistIOFactory.GetInstance().GetPlaylistIO(testFileLocation);
+
+                Assert.AreEqual(3, theReader.FilePaths.Count);
+                foreach (string s in theReader.FilePaths) Assert.IsTrue(System.IO.File.Exists(s));
+            }
+            finally
+            {
+                File.Delete(testFileLocation);
+            }
+        }
+
+        [TestMethod]
+        public void PL_XSPF()
+        {
+            string testFileLocation = TestUtils.CopyFileAndReplace(TestUtils.GetResourceLocationRoot() + "_Playlists/playlist.xspf", "$PATH", TestUtils.GetResourceLocationRoot(false));
+
+            try
+            {
+                IPlaylistIO theReader = PlaylistIOFactory.GetInstance().GetPlaylistIO(testFileLocation);
+
+                Assert.AreEqual(4, theReader.FilePaths.Count);
+                foreach (string s in theReader.FilePaths) Assert.IsTrue(System.IO.File.Exists(s));
+            }
+            finally
+            {
+                File.Delete(testFileLocation);
+            }
+        }
+
+        [TestMethod]
+        public void PL_SMIL()
+        {
+            IList<KeyValuePair<string, string>> replacements = new List<KeyValuePair<string, string>>();
+            string resourceRoot = TestUtils.GetResourceLocationRoot(false);
+            replacements.Add(new KeyValuePair<string, string>("$PATH", resourceRoot));
+            replacements.Add(new KeyValuePair<string, string>("$NODISK_PATH", resourceRoot.Substring(2, resourceRoot.Length - 2)));
+
+            string testFileLocation = TestUtils.CopyFileAndReplace(TestUtils.GetResourceLocationRoot() + "_Playlists/playlist.smil", replacements);
+            try
+            {
+                IPlaylistIO theReader = PlaylistIOFactory.GetInstance().GetPlaylistIO(testFileLocation);
+
+                Assert.AreEqual(3, theReader.FilePaths.Count);
+                foreach (string s in theReader.FilePaths) Assert.IsTrue(System.IO.File.Exists(s));
+            }
+            finally
+            {
+                File.Delete(testFileLocation);
+            }
+        }
+
+        [TestMethod]
+        public void PL_ASX()
+        {
+            string testFileLocation = TestUtils.CopyFileAndReplace(TestUtils.GetResourceLocationRoot() + "_Playlists/playlist.asx", "$PATH", TestUtils.GetResourceLocationRoot(false).Replace("\\", "/"));
+
+            try
+            {
+
+                IPlaylistIO theReader = PlaylistIOFactory.GetInstance().GetPlaylistIO(testFileLocation);
+
+                Assert.AreEqual(4, theReader.FilePaths.Count);
+                foreach (string s in theReader.FilePaths) Assert.IsTrue(System.IO.File.Exists(s));
+            }
+            finally
+            {
+                File.Delete(testFileLocation);
+            }
+        }
+
+        [TestMethod]
+        public void PL_B4S()
+        {
+            string testFileLocation = TestUtils.CopyFileAndReplace(TestUtils.GetResourceLocationRoot() + "_Playlists/playlist.b4s", "$PATH", TestUtils.GetResourceLocationRoot(false));
+
+            try
+            {
+                IPlaylistIO theReader = PlaylistIOFactory.GetInstance().GetPlaylistIO(testFileLocation);
+
+                Assert.AreEqual(3, theReader.FilePaths.Count);
+                foreach (string s in theReader.FilePaths) Assert.IsTrue(System.IO.File.Exists(s));
+            }
+            finally
+            {
+                File.Delete(testFileLocation);
+            }
+        }
+
+        [TestMethod]
+        public void PL_PLS()
+        {
+            string testFileLocation = TestUtils.CopyFileAndReplace(TestUtils.GetResourceLocationRoot() + "_Playlists/playlist.pls", "$PATH", TestUtils.GetResourceLocationRoot(false));
+
+            try
+            {
+                IPlaylistIO theReader = PlaylistIOFactory.GetInstance().GetPlaylistIO(testFileLocation);
+
+                Assert.AreEqual(4, theReader.FilePaths.Count);
+                foreach (string s in theReader.FilePaths) Assert.IsTrue(System.IO.File.Exists(s));
+            }
+            finally
+            {
+                File.Delete(testFileLocation);
+            }
+        }
+
+        [TestMethod]
+        public void PL_FPL()
+        {
+            string testFileLocation = TestUtils.CopyFileAndReplace(TestUtils.GetResourceLocationRoot() + "_Playlists/playlist.fpl", "$PATH", TestUtils.GetResourceLocationRoot(false));
+
+            try
+            {
+                IPlaylistIO theReader = PlaylistIOFactory.GetInstance().GetPlaylistIO(testFileLocation);
+
+                Assert.AreEqual(4, theReader.FilePaths.Count);
+                foreach (string s in theReader.FilePaths) Assert.IsTrue(System.IO.File.Exists(s));
+            }
+            finally
+            {
+                File.Delete(testFileLocation);
             }
         }
     }
