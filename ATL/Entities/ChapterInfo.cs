4f6913d911a844c0e6305f77f9591d8926306162
Zeugma440 <Zeugma440@users.noreply.github.com>
2020-10-25 19:00:02 +0100

    Documentation

--- ATL/Entities/ChapterInfo.cs
+++ ATL/Entities/ChapterInfo.cs
@@ -1,52 +1,128 @@
 ﻿namespace ATL
 {
+    /// <summary>
+    /// Information describing a chapter
+    /// </summary>
     public class ChapterInfo
     {
+        /// <summary>
+        /// Information describing an URL
+        /// </summary>
         public class UrlInfo
         {
+            /// <summary>
+            /// Description
+            /// </summary>
             public string Description { get; set; }
+            /// <summary>
+            /// The URL itself
+            /// </summary>
             public string Url { get; set; }
 
+            /// <summary>
+            /// Construct by copying data from the given UrlInfo
+            /// </summary>
+            /// <param name="url">Object to copy data from</param>
             public UrlInfo(UrlInfo url)
             {
                 Description = url.Description;
                 Url = url.Url;
             }
 
+            /// <summary>
+            /// Construct the structure from the URL and its description
+            /// </summary>
+            /// <param name="description">Description</param>
+            /// <param name="url">The URL itself</param>
             public UrlInfo(string description, string url)
             {
                 Description = description;
                 Url = url;
             }
 
+            /// <summary>
+            /// Construct the structure from a single URL
+            /// </summary>
+            /// <param name="url">The URL itself</param>
             public UrlInfo(string url)
             {
                 Description = "";
                 Url = url;
             }
 
+            /// <summary>
+            /// Internal string representation of the structure
+            /// </summary>
+            /// <returns>Internal string representation of the structure</returns>
             public override string ToString()
             {
                 return Description + Settings.InternalValueSeparator + Url;
             }
         }
 
-        public uint StartTime { get; set; }     // Start time (ms)
-        public uint EndTime { get; set; }       // End time (ms)
-        public uint StartOffset { get; set; }   // Start offset (bytes)
-        public uint EndOffset { get; set; }     // End offset (bytes)
-        public bool UseOffset { get; set; }     // True if StartOffset / EndOffset are usable, false if not
+        /// <summary>
+        /// Start time (ms)
+        /// NB : Only used when <see cref="UseOffset"/> is false
+        /// </summary>
+        public uint StartTime { get; set; }
+        /// <summary>
+        /// End time (ms)
+        /// NB : Only used when <see cref="UseOffset"/> is false
+        /// </summary>
+        public uint EndTime { get; set; }
 
+        /// <summary>
+        /// Start offset (bytes)
+        /// NB1 : Only used when <see cref="UseOffset"/> is true
+        /// NB2 : Only supported by ID3v2
+        /// </summary>
+        public uint StartOffset { get; set; }
+        /// <summary>
+        /// End offset (bytes)
+        /// NB1 : Only used when <see cref="UseOffset"/> is true
+        /// NB2 : Only supported by ID3v2
+        /// </summary>
+        public uint EndOffset { get; set; }
+
+        /// <summary>
+        /// True to use StartOffset / EndOffset instead of StartTime / EndTime
+        /// NB : Only supported by ID3v2
+        /// Default : false
+        /// </summary>
+        public bool UseOffset { get; set; }
+
+        /// <summary>
+        /// Title
+        /// </summary>
         public string Title { get; set; }
 
-        public string UniqueID { get; set; }    // Specific to ID3v2
-        public string Subtitle { get; set; }    // Specific to ID3v2
-        public UrlInfo Url { get; set; }        // Specific to ID3v2
-        public PictureInfo Picture { get; set; }// Specific to ID3v2
+        /// <summary>
+        /// Unique ID
+        /// NB : Only supported by ID3v2
+        /// </summary>
+        public string UniqueID { get; set; }
+        /// <summary>
+        /// Subtitle
+        /// NB : Only supported by ID3v2
+        /// </summary>
+        public string Subtitle { get; set; }
+        /// <summary>
+        /// Associated URL
+        /// NB : Only supported by ID3v2
+        /// </summary>
+        public UrlInfo Url { get; set; }
+        /// <summary>
+        /// Associated picture
+        /// NB : Only supported by ID3v2
+        /// </summary>
+        public PictureInfo Picture { get; set; }
 
 
         // ---------------- CONSTRUCTORS
 
+        /// <summary>
+        /// Construct an empty structure
+        /// </summary>
         public ChapterInfo()
         {
             StartTime = 0;
@@ -61,6 +137,10 @@
             Picture = null;
         }
 
+        /// <summary>
+        /// Construct a structure by copying informatio from the given ChapterInfo
+        /// </summary>
+        /// <param name="chapter">Structure to copy information from</param>
         public ChapterInfo(ChapterInfo chapter)
         {
             StartTime = chapter.StartTime; EndTime = chapter.EndTime; StartOffset = chapter.StartOffset; EndOffset = chapter.EndOffset; Title = chapter.Title; Subtitle = chapter.Subtitle; Url = chapter.Url; UniqueID = chapter.UniqueID;
--- ATL/Entities/LyricsInfo.cs
+++ ATL/Entities/LyricsInfo.cs
@@ -3,32 +3,86 @@ using System.Collections.Generic;
 
 namespace ATL
 {
+    /// <summary>
+    /// Information describing lyrics
+    /// </summary>
     public class LyricsInfo
     {
+        /// <summary>
+        /// Type (contents) of lyrics data
+        /// NB : Directly inspired by ID3v2 format
+        /// </summary>
         public enum LyricsType
         {
+            /// <summary>
+            /// Other (i.e. none of the other types of this enum)
+            /// </summary>
             OTHER = 0,
+            /// <summary>
+            /// Lyrical data
+            /// </summary>
             LYRICS = 1,
+            /// <summary>
+            /// Transcription
+            /// </summary>
             TRANSCRIPTION = 2,
+            /// <summary>
+            /// List of the movements in the piece
+            /// </summary>
             MOVEMENT_NAME = 3,
+            /// <summary>
+            /// Events that occur
+            /// </summary>
             EVENT = 4,
+            /// <summary>
+            /// Chord changes that occur in the music
+            /// </summary>
             CHORD = 5,
+            /// <summary>
+            /// Trivia or "pop up" information about the media
+            /// </summary>
             TRIVIA = 6,
+            /// <summary>
+            /// URLs for relevant webpages
+            /// </summary>
             WEBPAGE_URL = 7,
+            /// <summary>
+            /// URLs for relevant images
+            /// </summary>
             IMAGE_URL = 8
         }
 
+        /// <summary>
+        /// Phrase ("line") inside lyrics
+        /// </summary>
         public class LyricsPhrase
         {
+            /// <summary>
+            /// Timestamp of the phrase, in milliseconds
+            /// </summary>
             public int TimestampMs { get; set; }
+            /// <summary>
+            /// Text
+            /// </summary>
             public string Text { get; set; }
 
+            /// <summary>
+            /// Construct a lyrics phrase from its parts
+            /// </summary>
+            /// <param name="timestampMs">Timestamp, in milliseconds</param>
+            /// <param name="text">Text</param>
             public LyricsPhrase(int timestampMs, string text)
             {
                 TimestampMs = timestampMs;
                 Text = text;
             }
 
+            /// <summary>
+            /// Construct a lyrics phrase from its parts
+            /// </summary>
+            /// <param name="timestamp">Timestamp, in the form of a timecode
+            /// Supported formats : hh:mm, hh:mm:ss.ddd, mm:ss, hh:mm:ss and mm:ss.ddd</param>
+            /// <param name="text">Text</param>
             public LyricsPhrase(string timestamp, string text)
             {
                 TimestampMs = Utils.DecodeTimecodeToMs(timestamp);
@@ -36,14 +90,32 @@ namespace ATL
             }
         }
 
-        public string Description { get; set; }
-        public string LanguageCode { get; set; } // TODO - handle lyrics in multiple languages
-        public string UnsynchronizedLyrics { get; set; }
+        /// <summary>
+        /// Type
+        /// </summary>
         public LyricsType ContentType { get; set; }
+        /// <summary>
+        /// Description
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Language code
+        /// </summary>
+        public string LanguageCode { get; set; } // TODO - handle lyrics in multiple languages
+        /// <summary>
+        /// Data of unsynhronized (i.e. without associated timestamp) lyrics
+        /// </summary>
+        public string UnsynchronizedLyrics { get; set; }
+        /// <summary>
+        /// Data of unsynhronized (i.e. with associated timestamps) lyrics
+        /// </summary>
         public IList<LyricsPhrase> SynchronizedLyrics { get; set; }
 
         // ---------------- CONSTRUCTORS
 
+        /// <summary>
+        /// Construct empty lyrics information
+        /// </summary>
         public LyricsInfo()
         {
             Description = "";
@@ -53,6 +125,10 @@ namespace ATL
             SynchronizedLyrics = new List<LyricsPhrase>();
         }
 
+        /// <summary>
+        /// Construct lyrics information by copying data from the given LyricsInfo object
+        /// </summary>
+        /// <param name="info">Object to copy data from</param>
         public LyricsInfo(LyricsInfo info)
         {
             Description = info.Description;
