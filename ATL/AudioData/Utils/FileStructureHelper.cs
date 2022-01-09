619773b1eb85e4f5ed252eab167faef4a4a9f7c8
Zeugma440 <Zeugma440@users.noreply.github.com>
2022-01-09 14:53:53 +0100

    Look up the zones only once (thanks to JKamsker)

--- ATL/AudioData/Utils/FileStructureHelper.cs
+++ ATL/AudioData/Utils/FileStructureHelper.cs
@@ -498,7 +498,7 @@ namespace ATL.AudioData
 
 
             if (null == zones) return false;
-            if (!zones.ContainsKey(zone)) return true; // No effect
+            if (!zones.TryGetValue(zone, out Zone currentZone)) return true; // No effect
 
 
             // Get the dynamic correction map from the proper region
@@ -512,7 +512,7 @@ namespace ATL.AudioData
             bool isPostReprocessing = zone == POST_PROCESSING_ZONE_NAME;
 
             // == Update the current zone's headers
-            foreach (FrameHeader header in zones[zone].Headers)
+            foreach (FrameHeader header in currentZone.Headers)
             {
                 // === Update values
                 offsetPositionCorrection = -globalOffsetCorrection;
@@ -631,7 +631,7 @@ namespace ATL.AudioData
 
             // Update local dynamic offset if non-null
             if (deltaSize != 0 && !localDynamicOffsetCorrection.ContainsKey(zone))
-                localDynamicOffsetCorrection.Add(zone, new KeyValuePair<long, long>(zones[zone].Offset + zones[zone].Size, deltaSize));
+                localDynamicOffsetCorrection.Add(zone, new KeyValuePair<long, long>(currentZone.Offset + currentZone.Size, deltaSize));
 
             // If applicable, update global dynamic offset
             if (regionId > -1)
@@ -639,7 +639,7 @@ namespace ATL.AudioData
                 IDictionary<string, KeyValuePair<long, long>> globalRegion = dynamicOffsetCorrection[-1];
                 // Add new region
                 if (!globalRegion.ContainsKey(zone))
-                    globalRegion.Add(zone, new KeyValuePair<long, long>(zones[zone].Offset + zones[zone].Size, deltaSize));
+                    globalRegion.Add(zone, new KeyValuePair<long, long>(currentZone.Offset + currentZone.Size, deltaSize));
                 else // Increment current delta to existing region
                 {
                     KeyValuePair<long, long> currentValues = globalRegion[zone];
