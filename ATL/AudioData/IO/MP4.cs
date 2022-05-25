c735e62da72b0381dd5a5ecc9591e332929ef7db
Zeugma440 <Zeugma440@users.noreply.github.com>
2022-05-25 14:57:00 +0200

    Use vmhd for chapter pictures

--- ATL/AudioData/IO/MP4.cs
+++ ATL/AudioData/IO/MP4.cs
@@ -43,7 +43,7 @@ namespace ATL.AudioData.IO
         private const string ZONE_MP4_QT_CHAP_CHAP = "qt_chap";         // Quicktime chapters track reference
         private const string ZONE_MP4_QT_CHAP_TXT_TRAK = "qt_trak_txt"; // Quicktime chapters text track
         private const string ZONE_MP4_QT_CHAP_PIC_TRAK = "qt_trak_pic"; // Quicktime chapters picture track
-        private const string ZONE_MP4_QT_CHAP_MDAT = "qt_chap_mdat";    // Quicktime chapters data
+        private const string ZONE_MP4_QT_CHAP_TXT_MDAT = "qt_txt_mdat"; // Quicktime chapters text data
 
         private const string ZONE_MP4_PHYSICAL_CHUNK = "chunk";         // Physical audio chunk referenced from stco or co64
 
@@ -419,8 +419,8 @@ namespace ATL.AudioData.IO
                     structureHelper.AddZone(source.BaseStream.Position - 8, 0, ZONE_MP4_QT_CHAP_PIC_TRAK);
                     structureHelper.AddZone(source.BaseStream.Position - 8, 0, ZONE_MP4_QT_CHAP_TXT_TRAK);
                     // MDAT at the end of the file
-                    structureHelper.AddZone(source.BaseStream.Length, 0, ZONE_MP4_QT_CHAP_MDAT);
-                    structureHelper.AddSize(source.BaseStream.Length, 0, ZONE_MP4_QT_CHAP_MDAT, ZONE_MP4_QT_CHAP_MDAT);
+                    structureHelper.AddZone(source.BaseStream.Length, 0, ZONE_MP4_QT_CHAP_TXT_MDAT);
+                    structureHelper.AddSize(source.BaseStream.Length, 0, ZONE_MP4_QT_CHAP_TXT_MDAT, ZONE_MP4_QT_CHAP_TXT_MDAT);
                 }
             }
 
@@ -487,7 +487,7 @@ namespace ATL.AudioData.IO
                         structureHelper.RemoveZone(ZONE_MP4_QT_CHAP_CHAP);
                         structureHelper.RemoveZone(ZONE_MP4_QT_CHAP_TXT_TRAK);
                         structureHelper.RemoveZone(ZONE_MP4_QT_CHAP_PIC_TRAK);
-                        structureHelper.RemoveZone(ZONE_MP4_QT_CHAP_MDAT);
+                        structureHelper.RemoveZone(ZONE_MP4_QT_CHAP_TXT_MDAT);
                         LogDelegator.GetLogDelegate()(Log.LV_WARNING, "ATL does not support writing non-contiguous (e.g. interleaved with audio data) Quicktime chapters; ignoring Quicktime chapters.");
                         return;
                     }
@@ -501,9 +501,9 @@ namespace ATL.AudioData.IO
                     if (minChapterOffset >= source.BaseStream.Position && minChapterOffset < source.BaseStream.Position - 8 + mdatSize)
                     {
                         // Zone size = size of chapters
-                        structureHelper.AddZone(source.BaseStream.Position - 8, (int)chapterSize + 8, ZONE_MP4_QT_CHAP_MDAT);
+                        structureHelper.AddZone(source.BaseStream.Position - 8, (int)chapterSize + 8, ZONE_MP4_QT_CHAP_TXT_MDAT);
                         // Zone size header = actual size of the zone that may include audio data
-                        structureHelper.AddSize(source.BaseStream.Position - 8, mdatSize, ZONE_MP4_QT_CHAP_MDAT, ZONE_MP4_QT_CHAP_MDAT);
+                        structureHelper.AddSize(source.BaseStream.Position - 8, mdatSize, ZONE_MP4_QT_CHAP_TXT_MDAT, ZONE_MP4_QT_CHAP_TXT_MDAT);
                     }
 
                     source.BaseStream.Seek(mdatSize - 8, SeekOrigin.Current);
@@ -1523,9 +1523,9 @@ namespace ATL.AudioData.IO
             {
                 result = writeQTChaptersTrack(w, qtChapterPictureTrackNum, Chapters, globalTimeScale, Convert.ToUInt32(calculatedDurationMs), false);
             }
-            else if (zone.StartsWith(ZONE_MP4_QT_CHAP_MDAT)) // Quicktime chapter text data
+            else if (zone.StartsWith(ZONE_MP4_QT_CHAP_TXT_MDAT)) // Quicktime chapter text data
             {
-                result = writeQTChaptersData(w, Chapters);
+                result = writeQTChaptersTextData(w, Chapters);
             }
             else if (zone.StartsWith(ZONE_MP4_PHYSICAL_CHUNK)) // Audio chunks
             {
@@ -1892,11 +1892,11 @@ namespace ATL.AudioData.IO
             return 1;
         }
 
-        private int writeQTChaptersData(BinaryWriter w, IList<ChapterInfo> chapters)
+        private int writeQTChaptersTextData(BinaryWriter w, IList<ChapterInfo> chapters)
         {
             if (null == chapters || 0 == chapters.Count)
             {
-                structureHelper.RemoveZone(ZONE_MP4_QT_CHAP_MDAT); // Current zone commits suicide so that its size header doesn't get written
+                structureHelper.RemoveZone(ZONE_MP4_QT_CHAP_TXT_MDAT); // Current zone commits suicide so that its size header doesn't get written
                 return 0;
             }
 
@@ -2227,7 +2227,7 @@ namespace ATL.AudioData.IO
             //   - Child of the TRAK zone (i.e. won't be useful to process if the TRAK zone is deleted)
             // NB : Only works when QT track is located _before_ QT mdat
             string zoneId = ZONE_MP4_QT_CHAP_TXT_TRAK;
-            string dataZoneId = ZONE_MP4_QT_CHAP_MDAT;
+            string dataZoneId = ZONE_MP4_QT_CHAP_TXT_MDAT;
             Zone chapMdatZone = structureHelper.GetZone(dataZoneId);
 
             uint offset = (uint)(chapMdatZone.Offset + 8 + (isText ? 0 : totalTrackTxtSize));
