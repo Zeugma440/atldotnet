a10be9eb3e7d5a3bd6b24e9b9c2058e9fa37bbae
Zeugma440 <Zeugma440@users.noreply.github.com>
2023-10-01 14:04:31 +0200

    Remove duplicate unit test

--- ATL.unit-test/Playlist/PlaylistIOTest.cs
+++ ATL.unit-test/Playlist/PlaylistIOTest.cs
@@ -34,30 +34,6 @@ namespace ATL.test.IO.Playlist
         }
 
         [TestMethod]
-        public void PLIO_R_Common()
-        {
-            IPlaylistIO pls = PlaylistIOFactory.GetInstance().GetPlaylistIO(TestUtils.GetResourceLocationRoot() + "_Playlists/playlist_simple.m3u");
-
-            Assert.AreEqual(TestUtils.GetResourceLocationRoot() + "_Playlists/playlist_simple.m3u", pls.Path);
-
-            ArrayLogger log = new ArrayLogger();
-            try
-            {
-                pls = PlaylistIOFactory.GetInstance().GetPlaylistIO(TestUtils.GetResourceLocationRoot() + "_Playlists/efiufhziuefizeub.m3u");
-#pragma warning disable S1481 // Unused local variables should be removed
-                IList<string> files = pls.FilePaths;
-#pragma warning restore S1481 // Unused local variables should be removed
-                Assert.Fail();
-            }
-            catch
-            {
-                IList<LogItem> logItems = log.GetAllItems(Log.LV_ERROR);
-                Assert.AreEqual(2, logItems.Count); // Message and stacktrace
-                Assert.IsTrue(logItems[0].Message.Contains("efiufhziuefizeub.m3u")); // Can't do much more than than because the exception message is localized
-            }
-        }
-
-        [TestMethod]
         public void PL_Common()
         {
             IPlaylistIO theReader = PlaylistIOFactory.GetInstance().GetPlaylistIO(TestUtils.GetResourceLocationRoot() + "_Playlists/playlist_simple.m3u");
