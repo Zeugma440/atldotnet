bbdcd51fee334855c8589acad34b7d56cf9c6695
Zeugma440 <Zeugma440@users.noreply.github.com>
2024-01-06 21:34:10 +0100

    Add lyrics to unit test

--- ATL.unit-test/IO/TrackTest.cs
+++ ATL.unit-test/IO/TrackTest.cs
@@ -20,6 +20,11 @@ namespace ATL.test.IO
             referencePic.ComputePicHash();
             PictureInfo picture1 = referencePic;
             track.EmbeddedPictures.Add(picture1);
+            LyricsInfo lyrics = new LyricsInfo
+            {
+                UnsynchronizedLyrics = "lalala"
+            };
+            track.Lyrics = lyrics;
 
             // Test initial file is empty
             string testFileLocation = TestUtils.CopyAsTempTestFile("MP3/empty.mp3");
@@ -29,25 +34,26 @@ namespace ATL.test.IO
             Assert.AreEqual(0, track2.Year);
             Assert.AreEqual(0, track2.AdditionalFields.Count);
             Assert.AreEqual(0, track2.EmbeddedPictures.Count);
+            Assert.AreEqual(false, track2.Lyrics.Exists());
 
             // Test in-memory data after copy
             track.CopyMetadataTo(track2);
-            testCopiedMetadata(track2, referencePic);
+            testCopiedMetadata(track2, referencePic, lyrics);
 
             // Test deep copy
             track.AdditionalFields["CC"] = "ee";
             track.EmbeddedPictures[0] = PictureInfo.fromBinaryData(File.ReadAllBytes(TestUtils.GetResourceLocationRoot() + "_Images/pic2.jpeg"), PictureInfo.PIC_TYPE.Front);
-            testCopiedMetadata(track2, referencePic);
+            testCopiedMetadata(track2, referencePic, lyrics);
 
             // Test after saving
             track2.Save();
-            testCopiedMetadata(track2, referencePic);
+            testCopiedMetadata(track2, referencePic, lyrics);
 
             // Get rid of the working file
             if (Settings.DeleteAfterSuccess) File.Delete(testFileLocation);
         }
 
-        private void testCopiedMetadata(Track track, PictureInfo referencePic)
+        private void testCopiedMetadata(Track track, PictureInfo refPic, LyricsInfo refLyrics)
         {
             Assert.AreEqual("aaa", track.Title);
             Assert.AreEqual(1997, track.Year);
@@ -56,7 +62,8 @@ namespace ATL.test.IO
             Assert.AreEqual("dd", track.AdditionalFields["CC"]);
             Assert.AreEqual(1, track.EmbeddedPictures.Count);
             PictureInfo image = track.EmbeddedPictures[0];
-            Assert.AreEqual(referencePic.PictureHash, image.ComputePicHash());
+            Assert.AreEqual(refPic.PictureHash, image.ComputePicHash());
+            Assert.AreEqual(refLyrics.UnsynchronizedLyrics, track.Lyrics.UnsynchronizedLyrics);
         }
     }
 }
