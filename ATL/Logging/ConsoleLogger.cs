b3908ae58fe9be41cf13f8ed0f8833538517b30e
Zeugma440 <Zeugma440@users.noreply.github.com>
2025-10-24 10:57:43 +0200

    Add ConsoleLogger as part of ATL Core

---
+++ ATL/Logging/ConsoleLogger.cs
@@ -0,0 +1,28 @@
+﻿namespace ATL.Logging
+{
+    /// <summary>
+    /// Utility class to display ATL logging on the Console by simply calling `new ConsoleLogger();`
+    /// </summary>
+    public class ConsoleLogger : ILogDevice
+    {
+        private readonly Log theLog = new();
+        private long previousTimestamp = System.DateTime.Now.Ticks;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ConsoleLogger()
+        {
+            LogDelegator.SetLog(ref theLog);
+            theLog.Register(this);
+        }
+
+        void ILogDevice.DoLog(Log.LogItem anItem)
+        {
+            long delta_ms = (anItem.When.Ticks - previousTimestamp) / 10000; // Difference between last logging message, in ms
+            System.Console.WriteLine("[" + Log.getLevelName(anItem.Level) + "] " + anItem.Location + " | " + anItem.Message + " [Δ=" + delta_ms + " ms]");
+
+            previousTimestamp = anItem.When.Ticks;
+        }
+    }
+}
